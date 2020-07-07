79608191b815c5b655d49098d76995d71c50b9a9
otreblan <otreblain@gmail.com>
2020-07-06 20:53:26 -0500

    Fix comma

--- web/__init__.py
+++ web/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask,render_template, request, session, Response, redirect
+from flask import Flask, render_template, request, session, Response, redirect
 
 import json
 import time
