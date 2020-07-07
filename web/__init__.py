c9695fc767b8f1a9810dceac22dcd7e7c3cc1b0c
warleon <anthony.aguilar@utec.edu.pe>
2020-07-06 19:54:36 -0500

    .

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
-import flask
+from flask import Flask,render_template, request, session, Response, redirect
+from database import connector
+from model import entities
+import json
+import time
+import threading
+
+db = connector.Manager()
+engine = db.createEngine()
+app = Flask(__name__)
 
 
-def main():
-    print("Hello world")
+@app.route('/static/<content>')
+def static_content(content):
+    print(content)
+    return render_template(content)
 
 
-if __name__ == "__main":
-    main()
+@app.route('/', methods=['GET'])
+def getIndex():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.secret_key = ".."
+    app.run(port=8080, threaded=True, host=('127.0.0.1'))
+
---
+++ web/model/entities.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, Sequence, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from database import connector
+import datetime
+
+"""
+class User(connector.Manager.Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    name = Column(String(50))
+    fullname = Column(String(50))
+    password = Column(String(12))
+    username = Column(String(12))
+
+class Message(connector.Manager.Base):
+    __tablename__ = 'messages'
+    id = Column(Integer, Sequence('message_id_seq'), primary_key=True)
+    content = Column(String(500))
+    sent_on = Column(default=datetime.datetime.now())
+    user_from_id = Column(Integer, ForeignKey('users.id'))
+    user_to_id = Column(Integer, ForeignKey('users.id'))
+    user_from = relationship(User, foreign_keys=[user_from_id])
+    user_to = relationship(User, foreign_keys=[user_to_id])
+"""
+class Recipe(connector.Manager.Base):
+    __tablename__ = 'recipe'
+    id = Column(Integer, Sequence('recipe_id_seq'), primary_key=True)
+    Description = Column(String(600))
+    #           "ingredient":quantity
+#    Ingredients = Column({})
+    #       "step":time
+#    Steps = Column({})
+
