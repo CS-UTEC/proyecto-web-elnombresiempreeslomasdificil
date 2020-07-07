c0a56ad831fcc9f96696acaa2e6dd5304b3fbcf1
otreblan <otreblain@gmail.com>
2020-07-06 20:55:13 -0500

    setup.py line length exception

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Web",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/CS-UTEC/proyecto-web-elnombresiempreeslomasdificil",
+    url="https://github.com/CS-UTEC/proyecto-web-elnombresiempreeslomasdificil",  # noqa
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
