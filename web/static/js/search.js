57f2d381063c5cda6a2a9c3d4680a7eb3213dd68
otreblan <otreblain@gmail.com>
2020-07-22 13:58:07 -0500

    Query

--- web/static/js/search.js
+++ web/static/js/search.js
@@ -1,5 +1,20 @@
 function search() {
 	var query = $("#search").val()
 
-	console.log(query)
+	//console.log(query)
+
+	$.get({
+		url: '/recipes',
+		type: 'get',
+		dataType: 'json',
+		contentType: 'application/json',
+		success: function(data)
+		{
+			console.log(data)
+		},
+		error: function(data)
+		{
+			console.error("Error")
+		}
+	});
 }
