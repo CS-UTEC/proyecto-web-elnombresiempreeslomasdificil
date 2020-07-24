bf513c775562625b215f6d06d60d360c89563de1
jean.huby <50341023+jeanpaulHT@users.noreply.github.com>
2020-07-24 15:34:51 -0500

    search by id done

--- web/static/js/search_by_id.js
+++ web/static/js/search_by_id.js
@@ -1,4 +1,37 @@
 
+
+
+function search_id(id) {
+
+	$.get({
+		url: '/recipes2?user=' + id,
+		type: 'get',
+		dataType: 'json',
+		contentType: 'application/json',
+		success: function(data)
+		{
+
+			console.log(data)
+
+			for(i in  data){
+				var title = data[i].title
+				var id = data[i].id
+
+				console.log(title)
+				console.log(id)
+
+				$("#recipe_user").append(makeLink(title, id), "<br>")
+			}
+		},
+		error: function(data)
+		{
+			console.error("Error")
+		}
+	});
+}
+
+
+
 function get_current(){
 	console.log("Voy a traer al ususario");
 	$.getJSON("/current", function(data){
@@ -13,43 +46,4 @@ function get_current(){
 function makeLink(title, id) {
 	// Link placeholder
 	return $("<a>", {href: "/recipe_view.html?id="+id, text: title})
-}
-
-function search_id(id) {
-	var query = $("#search").val()
-
-	$.get({
-		url: '/recipes2/' + id,
-		type: 'get',
-		dataType: 'json',
-		contentType: 'application/json',
-		success: function(data)
-		{
-			$("#recipe_user").empty()
-			var options = {
-				includeScore: true,
-				keys : ['tags']
-			}
-
-			var fuse = new Fuse(data, options)
-
-			var results = fuse.search(query)
-
-			for(i in results){
-				var item = results[i].item
-
-				var title = item.title
-				var id = item.id
-
-				console.log(title)
-				console.log(id)
-
-				$("#recipes").append(makeLink(title, id), "<br>")
-			}
-		},
-		error: function(data)
-		{
-			console.error("Error")
-		}
-	});
 }
