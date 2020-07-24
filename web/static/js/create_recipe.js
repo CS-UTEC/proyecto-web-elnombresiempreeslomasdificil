bde51884824d5d6ecf129dfb80e3b7965e940656
jean.huby <50341023+jeanpaulHT@users.noreply.github.com>
2020-07-23 20:18:28 -0500

    cosas

--- web/static/js/create_recipe.js
+++ web/static/js/create_recipe.js
@@ -4,28 +4,24 @@ function create_recipe(){
     var url = $('#image').val()
     var ing = $('#Ingredients').tagsinput('items')
     var inst = $('#Instructions').tagsinput('items')
-
     
-    var tag = $('input');
-        elt.tagsinput({
 
-        insgredientes: {
-            source: ing
-        },
-        instrucciones: {
-            source: inst
-        }
+    var tag = $('input');
+    
+    tag.tagsinput({
+        itemDescription: "description",
+        itemIngredients: "Ingredients",
+        itemInstructions : "Instructions"
     });
 
+    tag.tagsinput('add', { "description": name , "Ingredients": ing   , "Instructions": inst});
+    console.log(tag)
 
-	var credentials = {
-		'title': name,
-		'markdown': pass,
-		'tags': pass,
+    var credential = {
+        "title": name,
+        "markdown": url,
+        "tags":tag
     };
-
-
-
 
 }
 
